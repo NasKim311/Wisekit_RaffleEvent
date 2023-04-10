c6ed41b2255a53861efe470be09bdb21f0c3fc7f
김남수 <skatn0311@gmail.com>
2023-04-10 09:27:44 +0900

    raffleRepository 수정

--- src/main/java/com/wisekit/assignment/repository/RaffleRepository.java
+++ src/main/java/com/wisekit/assignment/repository/RaffleRepository.java
@@ -11,6 +11,8 @@ import static com.wisekit.assignment.domain.QWinner.*;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class RaffleRepository {
@@ -21,6 +23,46 @@ public class RaffleRepository {
     //------------<addByLotData() / 당첨 데이터 저장 메소드>------------------------------------------------------------------------------------
     public void addByLotData(Winner winnerdata) {
         em.persist(winnerdata);
+
+    }
+
+    //------------<firstWinnerCount() / 1등 데이터를 리턴하는 메소드>------------------------------------------------------------------------------------
+    public int firstWinnerCount() {
+        queryFactory = new JPAQueryFactory(em);
+
+        List<Winner> firstWinnerData = queryFactory.selectFrom(winner).where(winner.winnerRank.eq(1)).fetch();
+
+        return firstWinnerData.size();
+
+    }
+
+    //------------<secondWinnerCount() / 2등 데이터를 리턴하는 메소드>------------------------------------------------------------------------------------
+    public int secondWinnerCount() {
+        queryFactory = new JPAQueryFactory(em);
+
+        List<Winner> secondWinnerData = queryFactory.selectFrom(winner).where(winner.winnerRank.eq(2)).fetch();
+
+        return secondWinnerData.size();
+
+    }
+
+    //------------<thirdWinnerCount() / 3등 데이터를 리턴하는 메소드>------------------------------------------------------------------------------------
+    public int thirdWinnerCount() {
+        queryFactory = new JPAQueryFactory(em);
+
+        List<Winner> thirdWinnerData = queryFactory.selectFrom(winner).where(winner.winnerRank.eq(3)).fetch();
+
+        return thirdWinnerData.size();
+
+    }
+
+    //------------<fourthWinnerCount() / 4등 데이터를 리턴하는 메소드>------------------------------------------------------------------------------------
+    public int fourthWinnerCount() {
+        queryFactory = new JPAQueryFactory(em);
+
+        List<Winner> thirdWinnerData = queryFactory.selectFrom(winner).where(winner.winnerRank.eq(4)).fetch();
+
+        return thirdWinnerData.size();
 
     }
 
